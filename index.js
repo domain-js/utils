a45f8ed033508836421767c214dbe6041d573215
Redstone Zhao <13740080@qq.com>
2021-04-01 18:18:45 +0800

    升级到 0.3.0

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const Logger = require("@open-node/logger");
 
 function Main(cnf, deps) {
-  const { logger } = cnf;
+  const { logger, clientId } = cnf;
 
-  return new Logger(logger, deps);
+  return new Logger(logger, deps, clientId);
 }
 
 Main.Deps = ["_"];

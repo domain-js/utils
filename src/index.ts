67748c3115d0b4df7aa3dd65be7e8aba901925ae
Redstone Zhao <13740080@qq.com>
2021-09-29 12:10:14 +0800

    完成utils的ts改写

--- src/index.ts
+++ src/index.ts
@@ -1,124 +1,106 @@
-import * as fs from "fs";
-import * as path from "path";
-// eslint-disable-next-line import/no-extraneous-dependencies
-import * as lodash from "lodash";
-import { v4 as uuid } from "uuid";
+import * as crypto from "crypto";
+import * as _ from "lodash";
 
-const date = (offset = 0) => new Date(Date.now() + (offset | 0)).toISOString();
+/** 随机字符串字典 */
+const RAND_STR_DICT = {
+  normal: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+  strong:
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&’()*+,-./:;<=>?@[]^_`{|}~",
+};
 
-interface Cnf {
-  logger: {
-    clientId: string;
-    errorLogPath: string;
-    infoLogPath: string;
-    ignoreErrors?: [string | number];
-  };
-}
+/** 计算给定字符串的md5值 */
+export const md5 = (str: string) => {
+  const hash = crypto.createHash("md5");
+  return hash.update(str).digest().toString("hex");
+};
 
-interface Deps {
-  _: typeof lodash;
-}
+/* 生成随机字符串 */
+export const randStr = (len: number, type = "normal") => {
+  const dict = type === "strong" || type === "normal" ? RAND_STR_DICT[type] : type;
+  const { length } = dict;
 
-interface Err extends Error {
-  code?: string | number;
-  data?: any;
-}
+  /** 随机字符串的长度不能等于 0 或者负数 */
+  len |= 0;
+  len = Math.max(len, 3);
 
-export function Main(cnf: Cnf, deps: Deps) {
-  const {
-    logger: { errorLogPath, infoLogPath, ignoreErrors, clientId },
-  } = cnf;
-  const { _ } = deps;
-  const makeDir = _.memoize((dir) => {
-    if (!fs.existsSync(dir)) fs.mkdirSync(dir);
-  });
+  return _.range(len)
+    .map(() => dict[Math.floor(Math.random() * length)])
+    .join("");
+};
 
-  const ignores = new Set(ignoreErrors);
+/** 将字符串里的换行，制表符替换为普通空格 */
+export const nt2space = (value: string) => {
+  if (typeof value !== "string") return value;
+  return value.replace(/(\\[ntrfv]|\s)+/g, " ").trim();
+};
 
-  const error = (e: Err, extra?: any) => {
-    if (!e) {
-      console.trace("Logger.error but error is null or undefined");
-      return;
-    }
-    const time = date();
-    const today = time.split("T")[0];
-    const code = e.code || "unknown";
-    // 忽略某些错误
-    if (ignores.has(code)) return;
-    const dir = path.resolve(errorLogPath, today);
-    makeDir(dir);
-    const file = path.resolve(dir, `${code}.err`);
-    const content = [time, clientId, e.message];
-    if (e.data) content.push(JSON.stringify(e.data));
-    if (extra != null) content.push(JSON.stringify(extra));
-    if (e.stack) content.push(JSON.stringify(e.stack));
+/** 首字符大写 */
+export const ucfirst = (value: string) => {
+  if (typeof value !== "string") return value;
+  return value[0].toUpperCase() + value.substring(1);
+};
+
+export const lcfirst = (value: string) => {
+  if (typeof value !== "string") return value;
+  return value[0].toLowerCase() + value.substring(1);
+};
+
+/** 睡眠等待 */
+export const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+/** 深度冻结一个对象，防止被不小心篡改 */
+export const deepFreeze = (object: any) => {
+  // Retrieve the property names defined on object
+  const propNames = Object.getOwnPropertyNames(object);
+
+  for (const name of propNames) {
+    const value = object[name];
+
+    object[name] = value && typeof value === "object" ? deepFreeze(value) : value;
+  }
+
+  return Object.freeze(object);
+};
+
+/** try catch 包裹执行, 记录错误日志 */
+export const tryCatchLog =
+  <T extends (...args: any[]) => any, L extends (...args: any[]) => any>(fn: T, errorLog: L) =>
+  async (...args: Parameters<T>): Promise<ReturnType<T> | Error> => {
     try {
-      fs.appendFileSync(file, `${content.join("\t")}\n`);
-    } catch (err) {
-      console.error("Logger.error appendFileSync faid: %o", err);
-    }
-  };
-
-  const info = (message: string, extra?: any) => {
-    const time = date();
-    const today = time.split("T")[0];
-    const dir = path.resolve(infoLogPath, today);
-    makeDir(dir);
-    const file = path.resolve(dir, "info.log");
-    const content = [time, clientId, message];
-    if (extra != null) content.push(JSON.stringify(extra));
-    try {
-      fs.appendFileSync(file, `${content.join("\t")}\n`);
+      const res = await fn(...args);
+      return res;
     } catch (e) {
-      console.error("Logger.info appendFileSync faid: %o", e);
+      errorLog(e);
+      return e as Error;
     }
   };
 
-  const logger = <T extends (...args: any[]) => any>(
-    fn: T,
-    name: string,
-    isAsync = true,
-    transform = (x: ReturnType<T>): any => x,
-    errorHandler = (e: Err | unknown) => (e instanceof Error ? e.message : ""),
-    argsHandler: (arg: Parameters<T>) => string = JSON.stringify,
-  ) => {
-    if (isAsync) {
-      return async (...args: Parameters<T>): Promise<ReturnType<T>> => {
-        const callId = uuid();
-        try {
-          info(`Begin: ${name}\t${callId}\t${argsHandler(args)}`);
-          const startedAt = Date.now();
-          const res = await fn(...args);
-          info(
-            `Completed: ${name}\t${callId}\t${Date.now() - startedAt}ms\t${JSON.stringify(
-              transform(res),
-            )}`,
-          );
-          return res;
-        } catch (e) {
-          info(`Error: ${name}\t${callId}\t${errorHandler(e)}`, e instanceof Error && e.stack);
-          throw e;
-        }
-      };
-    }
-    return (...args: Parameters<T>): ReturnType<T> => {
-      const callId = uuid();
-      try {
-        info(`Begin: ${name}\t${callId}\t${argsHandler(args)}`);
-        const startedAt = Date.now();
-        const res = fn(...args);
-        info(`Completed: ${name}\t${callId}\t${Date.now() - startedAt}ms\t${JSON.stringify(res)}`);
-        return res;
-      } catch (e) {
-        if (e instanceof Error) {
-          info(`Error: ${name}\t${callId}\t${e.message}`, e.stack);
-        } else {
-          info(`Error: ${name}\t${callId}\t${e}`, e);
-        }
-        throw e;
-      }
-    };
-  };
+/** 判断某个时间是否已过期，基于当前时间 */
+export const inExpired = (time: Date, life: number) => {
+  const now = (Date.now() / 1000) | 0;
 
-  return { error, info, logger };
-}
+  return time.valueOf() < now - life;
+};
+
+type Params = { [K: string]: string };
+
+/** 修改指定url上添加一些参数 */
+export const modifiyURL = (address: string, adds: Params, removes?: string[]) => {
+  const obj = new URL(address);
+
+  if (_.isObject(adds)) {
+    for (const key of Object.keys(adds)) {
+      obj.searchParams.append(key, adds[key]);
+    }
+  }
+
+  if (Array.isArray(removes)) {
+    for (const key of removes) obj.searchParams.delete(key);
+  }
+
+  return obj.toString();
+};
+
+export const isTest = () => process.env.NODE_ENV === "test";
+
+export const isProd = () => process.env.NODE_ENV === "production";
